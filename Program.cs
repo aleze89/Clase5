11a2797614d46be744490194079cf251a194e7a9
aleze89 <68794504+aleze89@users.noreply.github.com>
2022-05-17 00:14:06 -0300

    ejercicio de prueba de la clase 5, funciona....se podria hacer mejor??

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Ingrese su nombre por favor!");
+var nombreUsuario = Console.ReadLine();
+var respuesta = "";
+Console.WriteLine($"¡Hola {nombreUsuario}!");
+Console.WriteLine("Desea continuar?");
+respuesta = Console.ReadLine();
+
+while (respuesta.ToUpper() == "S")
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese su nombre por favor!");
+    nombreUsuario = Console.ReadLine();
+    Console.WriteLine($"¡Hola {nombreUsuario}!");
+    Console.WriteLine("Desea continuar?");
+    respuesta = Console.ReadLine();
+
+}
+
+if (respuesta.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado");
+}
+else
+{
+    Console.WriteLine("Opcion no valida!!!");
+}
+    
